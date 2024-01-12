d7450862a8ee0588a345b09037655bc8e9da1be0
Damian Othar <damianothar@gmail.com>
2024-01-12 07:36:27 -0300

    add WpAppBar.vue

---
+++ components/modules/WpAppBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar
+    color="primary"
+    density="compact"
+  >
+    <template #prepend>
+      <v-app-bar-nav-icon />
+    </template>
+    <v-app-bar-title>Ayrampo</v-app-bar-title>
+    <template #append>
+      <v-btn icon="mdi-dots-vertical" />
+    </template>
+  </v-app-bar>
+</template>
