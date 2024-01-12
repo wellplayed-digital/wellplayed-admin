c4e547f518ea8e1de5fa54f3b5665c5597d40c43
Damian Othar <damianothar@gmail.com>
2024-01-12 07:34:04 -0300

    add vuetify, eslint and stylelint

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-app-bar
+      color="primary"
+      density="compact"
+    >
+      <template #prepend>
+        <v-app-bar-nav-icon />
+      </template>
+
+      <v-app-bar-title>Ayrampo</v-app-bar-title>
+
+      <template #append>
+        <v-btn icon="mdi-dots-vertical" />
+      </template>
+    </v-app-bar>
+    <slot />
+  </v-app>
+</template>
